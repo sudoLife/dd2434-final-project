0b57182ef87e423fe096798cf56a2584b4e2c666
Matvei Kazharov <matvei@kth.se>
2023-01-09 14:20:38 +0100

    Version adhering to authors' implementation
    A few things to note here:
    1. This uses the author's implementation's tricks: like eigenvec filter,
       and raising degree matrix to the power of -1 as opposed to -1/2.

--- NetMF.py
+++ NetMF.py
@@ -3,6 +3,9 @@ import networkx as nx
 from scipy import sparse
 from utils import load_data_into_graph
 import logging
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from color_communities import color_communities
 
 """
 Parameters:
@@ -15,7 +18,8 @@ Parameters:
 
 class NetMF:
     def __init__(self, G: nx.graph, window: int, neg_samples: int, embedding_size: int, logger=logging.getLogger("NetMF")):
-        self.A = nx.to_numpy_array(G)  # adjacency matrix
+        self.A = nx.to_numpy_array(
+            G, nodelist=np.sort(G.nodes()))  # adjacency matrix
         self.volume = np.sum(self.A)
         self.window = window
         self.neg_samples = neg_samples
@@ -42,16 +46,17 @@ class NetMF:
         M = S @ D_inv
 
         # M'
-        M = sparse.csr_array(np.maximum(M, 1))
+        M = np.maximum(M, 1)
         self.logger.info("Almost done...")
 
-        return self.svd(M)
+        return self.svd(np.log(M))
 
     def large_window_size(self, rank):
         self.logger.info("Computing for large window size")
         laplacian, D = sparse.csgraph.laplacian(
             self.A, return_diag=True, normed=True)
-        D_inv_sqrt = sparse.diags(D ** -0.5)
+        # NOTE: this comes from the authors
+        D_inv_sqrt = sparse.diags(D ** -1.0)
         # D^-1/2 A D^-1/2
         D_A_D = sparse.identity(self.n) - laplacian
 
@@ -61,6 +66,8 @@ class NetMF:
         # Gives you eigenvals in increasing order tho
         # LA means it returns first `rank` largest eigenvalues
         eigenvals, eigenvecs = sparse.linalg.eigsh(D_A_D, rank, which='LA')
+        # NOTE: this comes from the authors' implementation for no good reason at all
+        eigenvals = self.deepwalk_filter(eigenvals)
 
         self.logger.info("Eigenvectors computed")
 
@@ -81,28 +88,53 @@ class NetMF:
         M = np.maximum(result, 1)
 
         self.logger.info("Almost done...")
-        return self.svd(M)
+        return self.svd(np.log(M))
+
+    def deepwalk_filter(self, evals):
+        for i in range(len(evals)):
+            x = evals[i]
+            evals[i] = 1. if x >= 1 else x * \
+                (1-x**self.window) / (1-x) / self.window
+        evals = np.maximum(evals, 0)
+        self.logger.info("After filtering, max eigenvalue=%f, min eigenvalue=%f",
+                         np.max(evals), np.min(evals))
+        return evals
 
     def svd(self, M):
         # TODO: lower precision of floats?
         # SVD
         u, s, _ = sparse.linalg.svds(
-            M, self.embedding_size, return_singular_vectors='u')
+            M, k=self.embedding_size, return_singular_vectors='u')
         # U_D
-        return u @ sparse.diags(np.sqrt(s))
+        # NOTE: another author's way of doing it
+        return sparse.diags(np.sqrt(s)).dot(u.T).T
 
 
 def main():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(message)s')  # include timestamp
-    G, _, _, _ = load_data_into_graph('BlogCatalog')
     logger = logging.getLogger(__name__)
+
+    # G = nx.karate_club_graph()
+    # netmf = NetMF(G, 5, 1, 32, logger=logger)
+    # embedding = netmf.small_window_size()
+    # x_pca = PCA(n_components=2).fit_transform(embedding)
+    # colors = color_communities(G)
+    # ax = plt.subplot(1, 1, 1)
+    # ax.scatter(x_pca[:, 0], x_pca[:, 1], c=colors)
+    # for node in G.nodes():
+    #     ax.annotate(
+    #         node, (x_pca[node, 0], x_pca[node, 1]))
+    # plt.show()
+
+    G, _, _, _ = load_data_into_graph('Flickr')
 
     netmf = NetMF(G, 10, 1, 128, logger=logger)
 
     # rank is selected from sec 4 in the paper.
-    embedding = netmf.large_window_size(256)
-    np.save("models/NetMF-BlogCatalog.npy", embedding, allow_pickle=False)
+    embedding = netmf.large_window_size(16384)
+    np.save("models/NetMF-Flickr-their-ordered.npy",
+            embedding, allow_pickle=False)
 
 
 if __name__ == "__main__":
